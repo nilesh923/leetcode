8a859f8413c9e613b3582c54eaa7a442e4efe5f8
nilesh923 <nileshmaheshwari136@gmail.com>
2026-07-08 13:18:40 +0530

    theory_recurrsion.java

--- theory_recurrsion.java
+++ theory_recurrsion.java
@@ -16,3 +16,26 @@ class Main {
         return a * facto(a-1);
     }
 }
+
+//#multiple of n*m
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Enter number: ");
+         int number = scanner.nextInt();
+         int number2 = scanner.nextInt();
+         //System.out.print(number);
+        // 3. Read a line of string input
+        
+        scanner.close();
+        System.out.print(mult(number,number2));
+        
+    }
+    public static int mult(int a,int b){
+        if(b==1)
+        return a;
+        else
+        return a + mult(a,b-1);
+    }
+}
